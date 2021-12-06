a903edad79ea9c98d7e53a6312279e1f1e0886f9
Coreline <a@nosov.org>
2021-12-06 16:14:47 +0700

    Added Shift and Ctrl hotkeys on Marker tools to straight line

--- PaintTools/PaintToolMarker.cs
+++ PaintTools/PaintToolMarker.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using System.Linq;
+using System.Drawing.Imaging;
 
 namespace Darkshot.PaintTools
 {
     class PaintToolMarker : PaintTool
     {
-        const int PEN_WIDTH = 24;
+        const float PEN_WIDTH = 24;
+        Cursor _cursor;
+        Point _pointLast;
         List<Point> _points;
         Pen _pen;
         bool _drawing;
@@ -16,9 +20,11 @@ namespace Darkshot.PaintTools
         public PaintToolMarker(Color color)
         {
             Paint += (s, e) => { onPaint(e.Graphics); };
-            MouseDown += (s, e) => { onMouseDown(e); };
-            MouseUp += (s, e) => { onMouseUp(e); };
-            MouseMove += (s, e) => { onMouseMove(e); };
+            MouseDown += (s, e) => { onMouseDown(s as Control, e); };
+            MouseUp += (s, e) => { onMouseUp(s as Control, e); };
+            MouseMove += (s, e) => { onMouseMove(s as Control, e); };
+            KeyDown += (s, e) => { refreshPoints(); };
+            KeyUp += (s, e) => { refreshPoints(); };
 
             _pen = new Pen(Color.FromArgb(127, color), PEN_WIDTH);
             _pen.StartCap = LineCap.Round;
@@ -26,6 +32,37 @@ namespace Darkshot.PaintTools
             _pen.LineJoin = LineJoin.Round;
             _points = new List<Point>();
             _drawing = false;
+
+            using (var bitmap = new Bitmap((int)PEN_WIDTH + 2, (int)PEN_WIDTH + 2, PixelFormat.Format32bppArgb))
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                var rect = new RectangleF(1, 1, bitmap.Width - 2, bitmap.Height - 2);
+                g.FillEllipse(_pen.Brush, rect);
+                g.FillEllipse(_pen.Brush, rect);
+                using (var pen = new Pen(color, 1))
+                    g.DrawEllipse(pen, rect);
+                _cursor = new Cursor(bitmap.GetHicon());
+            }
+        }
+
+        public override Rectangle GetBounds()
+        {
+            if (_points.Count == 0)
+                return SystemInformation.VirtualScreen;
+
+            var minX = _points.Min(p => p.X);
+            var minY = _points.Min(p => p.Y);
+            var maxX = _points.Max(p => p.X);
+            var maxY = _points.Max(p => p.Y);
+
+            var rect = new Rectangle(minX, minY, maxX - minX + 1, maxY - minY + 1);
+            rect.Inflate((int)PEN_WIDTH, (int)PEN_WIDTH);
+            return rect;
+        }
+
+        public override Cursor GetDefaultCursor()
+        {
+            return _cursor;
         }
 
         void onPaint(Graphics g)
@@ -33,29 +70,37 @@ namespace Darkshot.PaintTools
             if (_points.Count < 1)
                 return;
 
-            var points = _points.Count > 1
-                ? _points.ToArray()
-                : new Point[] { _points[0], _points[0] };
-            //var path = new GraphicsPath();
-            //path.AddLines(points);
-            //g.DrawPath(_pen, path);
-            g.DrawLines(_pen, points);
+            if (_points.Count == 1)
+            {
+                var w = PEN_WIDTH;
+                var r = w / 2;
+                var x = _points[0].X - r;
+                var y = _points[0].Y - r;
+                var rect = new RectangleF(x, y, w, w);
+                g.FillEllipse(_pen.Brush, rect);
+            }
+            else
+            {
+                g.DrawLines(_pen, _points.ToArray());
+            }
         }
 
-        void onMouseDown(MouseEventArgs e)
+        void onMouseDown(Control canvas, MouseEventArgs e)
         {
             _drawing = true;
             _points.Add(e.Location);
+            refreshPoints();
         }
 
-        void onMouseMove(MouseEventArgs e)
+        void onMouseMove(Control canvas, MouseEventArgs e)
         {
             if (!_drawing)
                 return;
-            _points.Add(e.Location);
+            _pointLast = e.Location;
+            refreshPoints();
         }
 
-        void onMouseUp(MouseEventArgs e)
+        void onMouseUp(Control canvas, MouseEventArgs e)
         {
             if (!_drawing)
                 return;
@@ -63,25 +108,38 @@ namespace Darkshot.PaintTools
             RaiseComplete();
         }
 
-        public override Rectangle GetBounds()
+        void refreshPoints()
         {
-            const int radius = 20;
-            if (_points.Count == 0)
-                return Rectangle.Empty;
+            if (_pointLast != Point.Empty && _pointLast != _points.Last())
+                _points.Add(_pointLast);
+            if (_points.Count < 2)
+                return;
+            if (!IsCtrlDown() && !IsShiftDown())
+                return;
 
-            var rect = new Rectangle(_points[0],Size.Empty);
-            foreach (var p in _points)
-            {
-                rect.X = Math.Min(rect.X, p.X);
-                rect.Y = Math.Min(rect.Y, p.Y);
-                rect.Width = Math.Abs(rect.X - p.X);
-                rect.Height = Math.Abs(rect.Y - p.Y);
-            }
-            rect.X -= radius;
-            rect.Y -= radius;
-            rect.Width += 2 * radius;
-            rect.Height += 2 * radius;
-            return rect;
+            var first = _points[0];
+            var last = _pointLast;
+            last = IsShiftDown() ? getStraightLine(first, last) : last;
+            _points.Clear();
+            _points.Add(first);
+            _points.Add(last);
+        }
+
+        Point getStraightLine(Point start, Point end)
+        {
+            var w = Math.Abs(end.X - start.X);
+            var h = Math.Abs(end.Y - start.Y);
+            var x = start.X + Math.Sign(end.X - start.X) * Math.Max(w, h);
+            var y = start.Y + Math.Sign(end.Y - start.Y) * Math.Max(w, h);
+            var t = (int)Math.Sqrt(Math.Pow(x - start.X, 2) + Math.Pow(y - start.Y, 2));
+            var z = (int)Math.Abs(t - Math.Sqrt(w * w + h * h));
+            var min = Math.Min(Math.Min(w, h), z);
+
+            if (min == w)
+                return new Point(start.X, end.Y);
+            if (min == h)
+                return new Point(end.X, start.Y);
+            return new Point(x, y);
         }
     }
 }
