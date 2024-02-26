7b8f4058c30a0f818bd05c10b790ee0bb4e3e45a
coreline <a@nosov.org>
2024-02-26 15:01:50 +0700

    blur bug fix

--- PaintTools/PaintToolBlur.cs
+++ PaintTools/PaintToolBlur.cs
@@ -1,8 +1,6 @@
 ﻿using Darkshot.Gdi32;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -11,7 +9,7 @@ namespace Darkshot.PaintTools
 {
     class PaintToolBlur : PaintTool
     {
-        const int RADIUS = 20;
+        const int RADIUS = 16;
         Rectangle _rect;
         Point _point;
         bool _drawing;
@@ -60,7 +58,7 @@ namespace Darkshot.PaintTools
 
             using (var bitmap = g.ToBitmap(rectOut))
             {
-                using (var blurred = Gaussian(bitmap, RADIUS))
+                using (var blurred = Blur(bitmap, RADIUS))
                 {
                     using (var croppedBlurred = blurred.Clone(rectIn, bitmap.PixelFormat))
                     {
@@ -112,9 +110,9 @@ namespace Darkshot.PaintTools
             return rect;
         }
 
-        Bitmap Gaussian(Bitmap bitmap, float sigma)
+        Bitmap Blur(Bitmap bitmap, float radius)
         {
-            if (bitmap.Width < 3 || bitmap.Height < 3 || sigma < 0.25f)
+            if (bitmap.Width < 3 || bitmap.Height < 3 || radius < 0.25f)
                 return bitmap;
 
             int w = bitmap.Width;
@@ -128,68 +126,63 @@ namespace Darkshot.PaintTools
             Marshal.Copy(bitmapData.Scan0, bytes, 0, bytes.Length);
             bitmap.UnlockBits(bitmapData);
 
-            var m = new float[w * h];
-            var r = new float[w * h];
-            var q = (float)((sigma <= 2.5)
-                             ? (3.97156 - 4.14554 * Math.Sqrt(1.0 - 0.26891 * Math.Sqrt(sigma)))
-                             : (-0.9633 + 0.98711 * Math.Sqrt(sigma)));
+            var l = w * h;
+            var m = new float[l];
+            var q = (float)((radius <= 2.5)
+                             ? (3.97156 - 4.14554 * Math.Sqrt(1.0 - 0.26891 * Math.Sqrt(radius)))
+                             : (-0.9633 + 0.98711 * Math.Sqrt(radius)));
             var b0 = (float)(1.57825 + 2.44413 * q + 1.4281 * q * q + 0.422205 * q * q * q);
             var b1 = (float)(2.44413 * q + 2.85619 * q * q + 1.26661 * q * q * q);
             var b2 = (float)(-1.4281 * q * q - 1.26661 * q * q * q);
             var b3 = (float)(0.422205 * q * q * q);
-            var k1 = (float)(1.0 - ((b1 + b2 + b3) / b0));
-            var k2 = (float)(k1 + (b1 + b2 + b3) / b0);
-            var k3 = (float)((b1 + b2 + b3) / b0);
+            var b4 = (float)(1.0 - ((b1 + b2 + b3) / b0));
+            var b5 = (float)(b4 + (b1 + b2 + b3) / b0);
+            var b6 = (float)((b1 + b2 + b3) / b0);
+            var b7 = (float)(b2 + b3);
+
 
             for (var c = 0; c < 3; c++)
             {
-                for (var i = 0; i < m.Length; i++)
-                {
-                    var y = i / w;
-                    var x = i % w;
-                    m[x * h + y] = 1f * bytes[i * ps + c];
-                }
+                for (var i = 0; i < l; i++)
+                    m[i] = 1f * bytes[i * ps + c];
 
-                for (int y = 0; y < h; y++)
+                for (int y = 0; y < l; y += w)
                 {
-                    r[y] = m[y] * k2;
-                    r[h + y] = k1 * m[h + y] + r[y] * k3;
-                    r[2 * h + y] = k1 * m[2 * h + y] + (b1 * r[h + y] + (b2 + b3) * r[y]) / b0;
+                    m[y] = m[y] * b5;
+                    m[y + 1] = b4 * m[y + 1] + m[y] * b6;
+                    m[y + 2] = b4 * m[y + 2] + (b1 * m[y + 1] + b7 * m[y]) / b0;
 
                     for (int x = 3; x < w; x++)
-                        r[x * h + y] = k1 * m[x * h + y] + (b1 * r[(x - 1) * h + y] + b2 * r[(x - 2) * h + y] + b3 * r[(x - 3) * h + y]) / b0;
+                        m[y + x] = b4 * m[y + x] + (b1 * m[y + x - 1] + b2 * m[y + x - 2] + b3 * m[y + x - 3]) / b0;
 
-                    r[(w - 1) * h + y] = r[(w - 1) * h + y] * k2;
-                    r[(w - 2) * h + y] = k1 * r[(w - 2) * h + y] + r[(w - 1) * h + y] * k3;
-                    r[(w - 3) * h + y] = k1 * r[(w - 3) * h + y] + (b1 * r[(w - 2) * h + y] + (b2 + b3) * r[(w - 1) * h + y]) / b0;
+                    m[y + w - 1] = m[y + w - 1] * b5;
+                    m[y + w - 2] = b4 * m[y + w - 2] + m[y + w - 1] * b6;
+                    m[y + w - 3] = b4 * m[y + w - 3] + (b1 * m[y + w - 2] + b7 * m[y + w - 1]) / b0;
 
                     for (int x = w - 4; x >= 0; x--)
-                        r[x * h + y] = k1 * r[x * h + y] + (b1 * r[(x + 1) * h + y] + b2 * r[(x + 2) * h + y] + b3 * r[(x + 3) * h + y]) / b0;
+                        m[y + x] = b4 * m[y + x] + (b1 * m[y + x + 1] + b2 * m[y + x + 2] + b3 * m[y + x + 3]) / b0;
                 }
 
                 for (int x = 0; x < w; x++)
                 {
-                    int row = x * h;
-                    r[row] = r[row] * k2;
-                    r[row + 1] = k1 * r[row + 1] + r[row] * k3;
-                    r[row + 2] = k1 * r[row + 2] + (b1 * r[row + 1] + (b2 + b3) * r[row]) / b0;
+                    m[x] = m[x] * b5;
+                    m[x + w] = b4 * m[x + w] + m[x] * b6;
+                    m[x + 2 * w] = b4 * m[x + 2 * w] + (b1 * m[x + w] + b7 * m[x]) / b0;
 
-                    for (int y = 3; y < h; y++)
-                        r[row + y] = k1 * r[row + y] + (b1 * r[row + y - 1] + b2 * r[row + y - 2] + b3 * r[row + y - 3]) / b0;
+                    for (int y = 3 * w + x; y < l; y += w)
+                        m[y] = b4 * m[y] + (b1 * m[y - w] + b2 * m[y - 2 * w] + b3 * m[y - 3 * w]) / b0;
 
-                    r[row + h - 1] = r[row + h - 1] * k2;
-                    r[row + h - 2] = k1 * r[row + h - 2] + r[row + h - 1] * k3;
-                    r[row + h - 3] = k1 * r[row + h - 3] + (b1 * r[row + h - 2] + (b2 + b3) * r[row + h - 1]) / b0;
+                    m[(h - 1) * w + x] = m[(h - 1) * w + x] * b5;
+                    m[(h - 2) * w + x] = b4 * m[(h - 2) * w + x] + m[(h - 1) * w + x] * b6;
+                    m[(h - 3) * w + x] = b4 * m[(h - 3) * w + x] + (b1 * m[(h - 2) * w + x] + b7 * m[(h - 1) * w + x]) / b0;
 
-                    for (int y = h - 4; y >= 0; y--)
-                        r[row + y] = k1 * r[row + y] + (b1 * r[row + y + 1] + b2 * r[row + y + 2] + b3 * r[row + y + 3]) / b0;
+                    for (int y = (h - 4) * w + x; y >= 0; y -= w)
+                        m[y] = b4 * m[y] + (b1 * m[y + w] + b2 * m[y + 2 * w] + b3 * m[y + 3 * w]) / b0;
                 }
 
-                for (var i = 0; i < r.Length; i++)
+                for (var i = 0; i < l; i++)
                 {
-                    var y = i / w;
-                    var x = i % w;
-                    var val = r[x * h + y];
+                    var val = m[i];
                     bytes[i * ps + c] = (byte)((val < 255) ? (val > 0 ? val : 0) : 255);
                 }
             }
